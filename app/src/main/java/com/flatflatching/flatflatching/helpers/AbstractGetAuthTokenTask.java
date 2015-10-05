2eec3a9a4d2df2f2f67c26dbc89149da0ea07d81
ValisStigma <rafael_krucker@hotmail.com>
2015-10-05 13:09:34 +0200

    added FlatService

---
+++ app/src/main/java/com/flatflatching/flatflatching/helpers/AbstractGetAuthTokenTask.java
@@ -0,0 +1,7 @@
+package com.flatflatching.flatflatching.helpers;
+
+/**
+ * Created by rafael on 05.10.2015.
+ */
+public class AbstractGetAuthTokenTask {
+}
---
+++ app/src/main/java/com/flatflatching/flatflatching/models/Address.java
@@ -0,0 +1,7 @@
+package com.flatflatching.flatflatching.models;
+
+/**
+ * Created by rafael on 05.10.2015.
+ */
+public class Address {
+}
