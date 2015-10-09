f2471bbb3d731c25f983ef9d3cb09ad642cf6572
ValisStigma <rafael_krucker@hotmail.com>
2015-10-09 09:29:12 +0200

    Refactoring

--- app/src/main/java/com/flatflatching/flatflatching/services/FlatService.java
+++ app/src/main/java/com/flatflatching/flatflatching/services/FlatService.java
@@ -10,6 +10,7 @@ import com.flatflatching.flatflatching.helpers.ServerConnector;
 import com.flatflatching.flatflatching.models.Flat;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,13 +20,124 @@ import java.util.List;
  */
 
 public class FlatService {
-    private static Flat currentFlat;
     private static String ownUrl = "";
+    private static String inviteUrl = "";
 
     public static void createFlat(Activity activity, TextView messageShower, ViewGroup viewContainer, String chosenEmail, Flat flat) {
-        currentFlat = flat;
-        new CreateFlatTask(activity, messageShower, viewContainer, chosenEmail, flat);
+        new CreateFlatTask(activity, messageShower, viewContainer, chosenEmail, flat).execute();
     }
+
+    public static void inviteFlatMate(Activity activity, TextView messageShower, ViewGroup viewContainer, String flatId, String email) {
+        new InviteFlatMateTask(activity, messageShower, viewContainer, flatId, email).execute();
+    }
+
+    private  static class InviteFlatMateTask extends AbstractGetAuthTokenTask {
+        private final String flatId;
+        private final String email;
+        private enum Status {
+            authFailed,
+            noAdmin,
+            userNotFound,
+            requestFailed,
+            notCompleted,
+            okay
+        }
+        private Status status;
+
+        public InviteFlatMateTask(Activity activity, TextView messageShower, ViewGroup viewContainer, String flatId, String email) {
+            super(activity, messageShower, viewContainer, inviteUrl);
+            this.flatId = flatId;
+            this.email = email;
+            status = Status.notCompleted;
+        }
+
+        @Override
+        protected void handleToken(String token) {
+            final String response = inviteFlatMate(token);
+            handleInviteResponse(response);
+        }
+
+        @Override
+        protected void postToken() {
+
+        }
+
+        private void handleInviteResponse(final String response) {
+            JSONObject result = null;
+            try {
+                result = new JSONObject(response);
+                String invite = result.getString("response");
+                status = Status.okay;
+            } catch (JSONException e) {
+                try {
+                    if(result == null) {
+                        status = Status.requestFailed;
+                    } else {
+                        int errorCode = result.getInt("error_code");
+                        switch (errorCode) {
+                            case 1:
+                                status = Status.authFailed;
+                                break;
+                            case 2:
+                                status = Status.noAdmin;
+                                break;
+                            case 11:
+                                status = Status.userNotFound;
+                                break;
+                        }
+                    }
+                } catch (JSONException | NullPointerException i) {
+                    status = Status.requestFailed;
+                }
+            }
+
+        }
+
+        private String inviteFlatMate(final String token) {
+            String params;
+            String result = "";
+            RequestBuilder requestBuilder = new RequestBuilder();
+            try {
+                params = requestBuilder.getInvitationRequest(token, flatId, email).toString();
+            } catch (JSONException e) {
+                return result;
+            }
+            final StringBuilder stringBuilder = new StringBuilder();
+            try {
+                final ServerConnector serverConnector = new ServerConnector(
+                        inviteUrl, "UTF-8");
+                serverConnector.addFormField("data", params);
+                final List<String> response = serverConnector.finish();
+                for (final String line : response) {
+                    stringBuilder.append(line);
+                }
+                result = stringBuilder.toString();
+            } catch (IOException i) {
+                result = "";
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+            super.onPostExecute(result);
+            switch (status) {
+                case authFailed:
+                    reactToError();
+                    break;
+                case noAdmin:
+                    reactToError();
+                    break;
+                case userNotFound:
+                    reactToError();
+                    break;
+                case notCompleted:
+                    reactToError();
+                    break;
+            }
+        }
+    }
+
 
     private static class CreateFlatTask extends AbstractGetAuthTokenTask {
         private Flat flat;
