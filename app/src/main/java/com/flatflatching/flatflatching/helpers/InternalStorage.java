3d6936a189bff29b5dfd217c5cb74ff38604e9e1
ValisStigma <rafael_krucker@hotmail.com>
2015-10-09 16:40:28 +0200

    Refactoring & Cleanup

---
+++ app/src/main/java/com/flatflatching/flatflatching/helpers/InternalStorage.java
@@ -0,0 +1,7 @@
+package com.flatflatching.flatflatching.helpers;
+
+/**
+ * Created by rafael on 09.10.2015.
+ */
+public class InternalStorage {
+}
---
+++ app/src/main/java/com/flatflatching/flatflatching/services/RequestService.java
@@ -0,0 +1,7 @@
+package com.flatflatching.flatflatching.services;
+
+/**
+ * Created by rafael on 09.10.2015.
+ */
+public class RequestService {
+}
