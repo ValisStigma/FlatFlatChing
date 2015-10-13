0b6fff76db92e5cc53d3bff797a65f2ea5d891a8
ValisStigma <rafael_krucker@hotmail.com>
2015-10-13 12:32:30 +0200

    Added toolbar

---
+++ app/src/main/java/com/flatflatching/flatflatching/models/StaticUserExpense.java
@@ -0,0 +1,7 @@
+package com.flatflatching.flatflatching.models;
+
+/**
+ * Created by rafael on 13.10.2015.
+ */
+public class StaticUserExpense {
+}
---
+++ app/src/main/java/com/flatflatching/flatflatching/services/ExpenseService.java
@@ -0,0 +1,7 @@
+package com.flatflatching.flatflatching.services;
+
+/**
+ * Created by rafael on 13.10.2015.
+ */
+public class ExpenseService {
+}
