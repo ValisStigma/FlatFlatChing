6b8e4642170f8a74ac80108f9f84cfe85eb33dc3
tobias <tschmitz@hsr.ch>
2015-10-19 13:46:50 +0200

    response error

--- Server/routes/flats.js
+++ Server/routes/flats.js
@@ -7,6 +7,7 @@ var flats = require(path.resolve("components/db-handler.js")).flats;
 var uuid = require("uuid");
 
 router.post("/create", userHandler.loggedIn, function (req, res, next) {
+    var error = false;
     if (!req.body.flat_name) {
         res.json(errors.auth.failed);
         res.end();
@@ -16,34 +17,41 @@ router.post("/create", userHandler.loggedIn, function (req, res, next) {
         if (!req.body.flat_address.flat_address_street) {
             res.json(errors.flat_address.street);
             res.end();
+            error = true;
         }
         if (!req.body.flat_address.flat_address_number) {
             res.json(errors.flat_address.number);
             res.end();
+            error = true;
         }
         if (!req.body.flat_address.flat_address_plz) {
             res.json(errors.flat_address.plz);
             res.end();
+            error = true;
         }
         if (!req.body.flat_address.flat_address_place) {
             res.json(errors.flat_address.place);
             res.end();
+            error = true;
         }
         if (!req.body.flat_address.flat_address_land) {
             res.json(errors.flat_address.land);
             res.end();
+            error = true;
         }
         flat_address = req.body.flat_address;
     }
-    flats.insert({
-        flat_name:req.body.flat_name,
-        flat_address: flat_address,
-        flat_uuid: uuid.v4()
-    }, function(err, flat){
-        if(!err){
-            res.json({flat_uuid: flat.flat_uuid});
-        }
-    });
+    if(!error) {
+        flats.insert({
+            flat_name: req.body.flat_name,
+            flat_address: flat_address,
+            flat_uuid: uuid.v4()
+        }, function (err, flat) {
+            if (!err) {
+                res.json({flat_uuid: flat.flat_uuid});
+            }
+        });
+    }
 });
 
 module.exports = router;
