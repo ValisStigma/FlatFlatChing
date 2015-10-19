9645a21879fd30753f49b83c9ff02c451153fe93
tobias <tschmitz@hsr.ch>
2015-10-19 15:14:35 +0200

    qfix

--- Server/routes/flats.js
+++ Server/routes/flats.js
@@ -10,47 +10,39 @@ router.post("/create", userHandler.loggedIn, function (req, res, next) {
     var error = false;
     if (!req.body.flat_name) {
         res.json(errors.auth.failed);
-        res.end();
-    }
-    var flat_address = {};
-    if (req.body.flat_address) {
-        if (!req.body.flat_address.flat_address_street) {
-            res.json(errors.flat_address.street);
-            res.end();
-            error = true;
-        }
-        if (!req.body.flat_address.flat_address_number) {
-            res.json(errors.flat_address.number);
-            res.end();
-            error = true;
-        }
-        if (!req.body.flat_address.flat_address_plz) {
-            res.json(errors.flat_address.plz);
-            res.end();
-            error = true;
-        }
-        if (!req.body.flat_address.flat_address_place) {
-            res.json(errors.flat_address.place);
-            res.end();
-            error = true;
-        }
-        if (!req.body.flat_address.flat_address_land) {
-            res.json(errors.flat_address.land);
-            res.end();
-            error = true;
-        }
-        flat_address = req.body.flat_address;
-    }
-    if(req.body.flat_name && !error) {
-        flats.insert({
-            flat_name: req.body.flat_name,
-            flat_address: flat_address,
-            flat_uuid: uuid.v4()
-        }, function (err, flat) {
-            if (!err) {
-                res.json({flat_uuid: flat.flat_uuid});
+    }else {
+        var flat_address = {};
+        if (req.body.flat_address) {
+            if (!req.body.flat_address.flat_address_street) {
+                res.json(errors.flat_address.street);
+                error = true;
+            } else if (!req.body.flat_address.flat_address_number) {
+                res.json(errors.flat_address.number);
+                error = true;
+            } else if (!req.body.flat_address.flat_address_plz) {
+                res.json(errors.flat_address.plz);
+                error = true;
+            } else if (!req.body.flat_address.flat_address_place) {
+                res.json(errors.flat_address.place);
+                error = true;
+            } else if (!req.body.flat_address.flat_address_land) {
+                res.json(errors.flat_address.land);
+                error = true;
             }
-        });
+            flat_address = req.body.flat_address;
+
+            if (!error) {
+                flats.insert({
+                    flat_name: req.body.flat_name,
+                    flat_address: flat_address,
+                    flat_uuid: uuid.v4()
+                }, function (err, flat) {
+                    if (!err) {
+                        res.json({flat_uuid: flat.flat_uuid});
+                    }
+                });
+            }
+        }
     }
 });
 
